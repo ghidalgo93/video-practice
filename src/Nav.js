e8d095fc6737b98e3c39eaf81b8e2053e1b2b0c6
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-02-13 09:03:43 -0700

    initial cm, start proj

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+const Nav = () =>{
+  const content = (
+    <nav>
+      <ul>
+        <li>logo</li>
+        <li>search baj</li>
+        <li>logo</li>
+        <li>profile pic</li>
+      </ul>
+    </nav>
+  )
+  
+  return content;
+}
+
+export default Nav;
